53bab0114f80882f209d9967b173b0741faae2e1
deepatesting <kspdeepa82@gmail.com>
2022-04-09 11:22:30 +0100

    Refactoring using parameterized test.

--- src/test/java/com/techreturners/marsrover/RoverTest.java
+++ src/test/java/com/techreturners/marsrover/RoverTest.java
@@ -2,31 +2,23 @@ package com.techreturners.marsrover;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class RoverTest {
 
-    @Test
-    public void checkFirstTest(){
+    @ParameterizedTest
+    @ValueSource(strings = {"0:0:N",
+                            "0:1:N"})
+    public void checkFirstTest(String initialPosition){
         //Arrange
-        Rover rover = new Rover("0:0:N");
+        Rover rover = new Rover(initialPosition);
 
         //Act
         String result = rover.getPosition();
 
         //Assert
-        Assertions.assertEquals("0:0:N", result);
-    }
-
-    @Test
-    public void checkSecondTest(){
-        //Arrange
-        Rover rover = new Rover("0:1:N");
-
-        //Act
-        String result = rover.getPosition();
-
-        //Assert
-        Assertions.assertEquals("0:1:N", result);
+        Assertions.assertEquals(initialPosition, result);
     }
 
 }
