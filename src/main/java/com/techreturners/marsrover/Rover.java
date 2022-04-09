69ff54eb2cc1538de6cefda4fd1d2553b8af277c
deepatesting <kspdeepa82@gmail.com>
2022-04-09 11:13:32 +0100

    Green commit - Second Test Pass.

--- src/main/java/com/techreturners/marsrover/Rover.java
+++ src/main/java/com/techreturners/marsrover/Rover.java
@@ -1,4 +1,14 @@
 package com.techreturners.marsrover;
 
 public class Rover {
+
+    private String position = "";
+
+    public Rover(String position){
+        this.position = position;
+    }
+
+    public String getPosition(){
+        return position;
+    }
 }
--- src/test/java/com/techreturners/marsrover/RoverTest.java
+++ src/test/java/com/techreturners/marsrover/RoverTest.java
@@ -1,4 +1,32 @@
 package com.techreturners.marsrover;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class RoverTest {
+
+    @Test
+    public void checkFirstTest(){
+        //Arrange
+        Rover rover = new Rover("0:0:N");
+
+        //Act
+        String result = rover.getPosition();
+
+        //Assert
+        Assertions.assertEquals("0:0:N", result);
+    }
+
+    @Test
+    public void checkSecondTest(){
+        //Arrange
+        Rover rover = new Rover("0:1:N");
+
+        //Act
+        String result = rover.getPosition();
+
+        //Assert
+        Assertions.assertEquals("0:1:N", result);
+    }
+
 }
