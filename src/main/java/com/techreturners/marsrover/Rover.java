623e0967c9eeab8c2fde711f6b074e0b45dca74a
deepatesting <kspdeepa82@gmail.com>
2022-04-10 09:22:48 +0100

    Written code to move forward.

--- src/main/java/com/techreturners/marsrover/Rover.java
+++ src/main/java/com/techreturners/marsrover/Rover.java
@@ -13,8 +13,13 @@ public class Rover {
     }
 
     public String moveToDirection(String command){
+        String[] initialPosition = position.split(":");
+        int x = Integer.parseInt(initialPosition[0]);
+        int y = Integer.parseInt(initialPosition[1]);
+        String direction = initialPosition[2];
+
         if(command == "M"){
-            return "0:1:N";
+            return (x+":"+(y+1)+":"+direction);
         }
         return "";
     }
